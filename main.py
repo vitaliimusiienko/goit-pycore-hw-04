db038ee67febb632beeb165b886c3192a6604ffb
Vitalii <vitaliimusiienko10@gmail.com>
2025-10-27 15:24:03 +0200

    change indentation

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def vizualize_directory_path(path: str, indent: int = 0):
         prefix = " " * indent
         if path.is_dir():
             print(Fore.BLUE + f"{prefix}{path.name}/")
-            vizualize_directory_path(path, indent + 4)
+            vizualize_directory_path(path, indent + 1)
         else:
             print(Fore.GREEN + f"{prefix}{path.name}")
             
